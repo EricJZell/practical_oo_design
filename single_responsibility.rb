5b91a8f84b4e24ce2c1766952d0b7f9d972e4a8b
Eric Zell <EricJZell@gmail.com>
2016-02-12 23:34:37 -0500

    Refactor and separate into two class: gear and wheel

--- single_responsibility.rb
+++ single_responsibility.rb
@@ -23,9 +23,11 @@ class RevealingReferences
   end
 
   def diameters
-    wheels.collect { |wheel|
-      wheel.rim + (wheel.tire * 2)
-    }
+    wheels.collect { |wheel| diameter(wheel) }
+  end
+
+  def diameter(wheel)
+    wheel.rim + (wheel.tire * 2)
   end
 
   Wheel = Struct.new(:rim, :tire)
@@ -35,16 +37,16 @@ class RevealingReferences
     }
   end
   # now if the data structure changes, only the wheelify method needs to be changed
+  # iteration of the wheels array is separated from the action of calculating the diameter
 end
 
 
 class Gear
-  attr_reader :chainring, :cog, :rim, :tire
-  def initialize(chainring, cog, rim, tire)
+  attr_reader :chainring, :cog, :wheel
+  def initialize(chainring, cog, wheel=nil)
     @chainring = chainring
     @cog = cog
-    @rim = rim
-    @tire = tire
+    @wheel = wheel
   end
 
   def ratio
@@ -52,6 +54,30 @@ class Gear
   end
 
   def gear_inches
-    ratio * (rim + (tire * 2))
+    ratio * wheel.diameter
   end
 end
+
+class Wheel
+  attr_reader :rim, :tire
+
+  def initialize(rim, tire)
+    @rim = rim
+    @tire = tire
+  end
+
+  def diameter
+    rim + (tire * 2)
+  end
+
+  def circumference
+    diameter * Math::PI
+  end
+end
+
+@wheel = Wheel.new(26, 1.5)
+puts @wheel.circumference
+
+puts Gear.new(52, 11, @wheel).gear_inches
+
+puts Gear.new(52, 11).ratio
